cfdbf156c41685a7e486f08d4e836fe3f0a68302
Neil Snyder <neil.r.snyder@gmail.comwq>
2025-01-11 17:27:56 -0800

    Adapt express .listen command to updated docker container networking moide
    Update redis connection method to use service name

--- app/index.js
+++ app/index.js
@@ -128,4 +128,4 @@ app.get('*', (req, res) => {
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => logger.info(`Server running on port ${PORT}`));
